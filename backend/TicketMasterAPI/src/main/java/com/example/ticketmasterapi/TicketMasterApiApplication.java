4ee85ab66fe07fa448890445adf414a5c66a1a21
kostenarov <dimitar.kostenarov@gmail.com>
2023-07-05 13:48:14 +0300

    initial commit and spring boot API config

---
+++ backend/TicketMasterAPI/src/main/java/com/example/ticketmasterapi/TicketMasterApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketmasterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketMasterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketMasterApiApplication.class, args);
+    }
+
+}
