fa640be9da5152581e92341acf24343e753bfe82
HristiyanPetkov <hristiyanpetkov05@gmail.com>
2023-07-10 16:13:16 +0300

    Limited autocomplete responses to 10

--- backend/TicketMasterAPI/src/main/java/com/example/ticketmasterapi/services/impl/LookupTableServiceImpl.java
+++ backend/TicketMasterAPI/src/main/java/com/example/ticketmasterapi/services/impl/LookupTableServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.ticketmasterapi.dto.LookupTableResource;
 import com.example.ticketmasterapi.models.LookupTableEntity;
 import com.example.ticketmasterapi.services.LookupTableService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,7 +17,8 @@ import static com.example.ticketmasterapi.mappers.LookupTableMapper.LOOKUP_TABLE
 public class LookupTableServiceImpl implements LookupTableService {
     private final LookupTableRepository lookupTableRepository;
 
-
+    @Value("${number.of.flights}")
+    private int limit;
     @Override
     public LookupTableResource save(LookupTableResource lookupTableResource) {
         return LOOKUP_TABLE_MAPPER.toLookupTableResource(lookupTableRepository.save(LOOKUP_TABLE_MAPPER.fromLookupTableResource(lookupTableResource)));
@@ -34,7 +36,7 @@ public class LookupTableServiceImpl implements LookupTableService {
 
     @Override
     public List<LookupTableResource> getAirportsByPartialAirport(String partialAirport) {
-        List<LookupTableEntity> lookupTableRepositoryList = lookupTableRepository.getAirportsByAirportContaining(partialAirport);
+        List<LookupTableEntity> lookupTableRepositoryList = lookupTableRepository.getAirportsByAirportContaining(partialAirport).stream().limit(limit).toList();
         return LOOKUP_TABLE_MAPPER.toLookupTableResources(lookupTableRepositoryList);
     }
 }
